bf048a6a8f4a274d78d0b40b22b073d32f7fb163
Kevin Lai <kevinlai@kevins-mbp.ait-sydney.net.au>
2017-09-13 15:01:22 +1000

    delete Q and add Qu

--- source/boggle_board.rb
+++ source/boggle_board.rb
@@ -1,25 +1,25 @@
 class BoggleBoard
-  def initialize
-    # how do we describe it
-    # creating an array of 16 underscores
-    @spaces = Array.new(16,"_")
+    def initialize
+      # how do we describe it
+      # creating an array of 16 underscores
+      @spaces = Array.new(16,"_")
 
-    # The 'to_a' simply tells Ruby: "to array".
-    @alphabet = ("A".."Z").to_a
-  end
+      # The 'to_a' simply tells Ruby: "to array".
+      @alphabet = ("A".."Z").to_a
+      @alphabet = @alphabet - ["Q"] + ["Qu"] # deleting Q # adding Qu
+    end
 
 # what does it do?
 # "!" can change the variable
-  def shake!
-    # take spaces
-    # REPLACE them with random numbers (a good idea is to create a VAR for this)
-    # split it into board
-    # call the shake method with Boggleboard class
-    # could use "MAP"
-    @spaces.map! do |space|
-      space = @alphabet.shuffle.take(1)
-    end
-    @spaces
+     def shake!
+      # take spaces
+      # REPLACE them with random numbers (a good idea is to create a VAR for this)
+      # split it into board
+      # call the shake method with Boggleboard class
+      # could use "MAP"
+      @spaces.map! do |space|
+        space = @alphabet.shuffle.take(1)
+      end
   end
 
   # Defining to_s on an object controls how the object is
@@ -27,16 +27,21 @@ class BoggleBoard
   def to_s
     # a new string for the output
     output_string = String.new
+
+    output_string = @spaces[0..3].join(' ') + "\n"
+    output_string = output_string + @spaces[4..7].join(' ') + "\n"
+    output_string = output_string + @spaces[8..11].join(' ') + "\n"
+    output_string = output_string + @spaces[12..15].join(' ') + "\n"
     # making a variable to mess with so i dont screw up my instance variable
-    letter_array = @spaces
+
 
     # how do i put them in for lines
 
-    # Run it for 4 times
-    4.times do
+    # # Run it for 4 times
+    # 4.times do
       # we are adding output_string to letter_array, "\n" means new line
-      output_string << letter_array.shift(4).join('') + "\n"
-    end
+    #   output_string << letter_array.shift(4).join('') + "\n"
+    # end
 
     # take the empty output_spring
     # add 4 items from letter_array to the output_string
@@ -47,12 +52,13 @@ class BoggleBoard
 
 
     # print the output_string
-    output_string
+    puts output_string
   end
 end
 
 
 
 board = BoggleBoard.new
-puts board.to_s
-puts board.shake!
+puts board
+board.shake!
+puts board
